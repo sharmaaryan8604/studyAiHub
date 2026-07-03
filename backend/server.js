e2c1c50d549eda45f1ce2cad4f36f617ad84632f
Aryan Sharma <sharma.aryan8604@gmail.com>
2026-07-03 11:39:11 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://study-ai-hub-psi.vercel.app/",
+      "https://study-ai-hub-psi.vercel.app",
     ],
     credentials: true,
   })
