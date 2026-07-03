7f27d14d818286f68083e90863d04af8f6dbba0f
Aryan Sharma <sharma.aryan8604@gmail.com>
2026-07-03 11:26:46 +0530

    feat: update axios configuration to use environment variable for base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Add JWT automatically
 api.interceptors.request.use((config) => {
-
     const token = localStorage.getItem("token");
 
     if (token) {
